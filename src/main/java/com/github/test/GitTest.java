fa6ecd3164c60efc5331bf71e22803a973986b04
github2056329480 <2056329480@qq.com>
2023-05-17 16:07:10 +0800

    主分支添加

--- src/main/java/com/github/test/GitTest.java
+++ src/main/java/com/github/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("更新1");
         System.out.println("创建分支dev");
         System.out.println("再次测试分支dev");
+        System.out.println("主分支添加");
     }
 }
