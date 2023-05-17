9888feb8851ec8351aeeaf04363e967e404fb1fe
github2056329480 <2056329480@qq.com>
2023-05-17 15:46:58 +0800

    第一次测试提交

---
+++ src/main/java/com/github/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.github.test;
+
+/**
+ * ClassName: GitTest
+ * Package: com.github.test
+ * Description:
+ *
+ * @Author:
+ * @Create:
+ * @version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
