cc7043917ec38e5a096310fce21ddaddfa795d54
github2056329480 <2056329480@qq.com>
2023-05-17 16:01:50 +0800

    分支提交

--- src/main/java/com/github/test/GitTest.java
+++ src/main/java/com/github/test/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("更新1");
+        System.out.println("创建分支dev");
+        System.out.println("再次测试分支dev");
     }
 }
